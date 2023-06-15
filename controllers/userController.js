7b2bdc4d72068d47e1ef0462fd534b95e9dc4386
unknown <manu9g@gmail.com>
2023-06-15 22:43:08 +0530

    userpayload passed

--- controllers/userController.js
+++ controllers/userController.js
@@ -87,7 +87,7 @@ const logIn = async (req, res) => {
       expiresIn: "1d",
     });
     res.cookie("jwt", token);
-    res.send({ status: "success", msg: "User Logged in Successfully" ,token});
+    res.send({ status: "success", msg: "User Logged in Successfully" ,token,userPayload});
   } catch (error) {
     res
       .status(500)
